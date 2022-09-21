73a94534f7da4d7961c41b51aef2b80d9ad766c6
VenusChanhk <46880256+VenusChanhk@users.noreply.github.com>
2022-09-21 17:06:00 +0800

    Update hk-sz-infrastructure-js.js

--- hk-sz-infrastructure-js.js
+++ hk-sz-infrastructure-js.js
@@ -1,32 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const poi_apollo = document.querySelector('#poi_apollo');
-    const poi_qianhai = document.querySelector('#poi_qianhai');
-    
-    const info_poi_apollo = document.querySelector('#info_poi_apollo');
-    const info_poi_qianhai = document.querySelector('#info_poi_qianhai');
+  const poi_apollo = document.querySelector('#poi_apollo');
+  const poi_qianhai = document.querySelector('#poi_qianhai');
+  
+  const info_poi_apollo = document.querySelector('#info_poi_apollo');
+  const info_poi_qianhai = document.querySelector('#info_poi_qianhai');
 
-    poi_apollo.addEventListener('click', function () {
-      info_poi_apollo.style.display = "block";
-      info_poi_qianhai.style.display = "none";
-    });
-    poi_qianhai.addEventListener('click', function () {
-      info_poi_apollo.style.display = "none";
-      info_poi_qianhai.style.display = "block";
-    });
+  function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  };
 
-      document.querySelector("#close-poi_1").addEventListener('click', function () {
-              info_poi_apollo.style.display = "none";
-    });
-      document.querySelector("#close-poi_2").addEventListener('click', function () {
-              info_poi_qianhai.style.display = "none";
-    });
+  poi_apollo.addEventListener('click', function () {
+    info_poi_apollo.style.display = "block";
+    info_poi_qianhai.style.display = "none";
+  });
+  poi_qianhai.addEventListener('click', function () {
+    info_poi_apollo.style.display = "none";
+    info_poi_qianhai.style.display = "block";
+    const popup_box_1_num1 = document.querySelector("#popup_box_1_num1");
+    const popup_box_3_num1 = document.querySelector("#popup_box_3_num1");
+    const popup_box_1_num3 = document.querySelector("#popup_box_1_num3");
+    const popup_box_4_num1 = document.querySelector("#popup_box_4_num1");
+    animateValue(popup_box_1_num1, 0, 120, 500);
+    animateValue(popup_box_1_num3, 0, 56, 500);
+    animateValue(popup_box_3_num1, 0, 12000, 500);
+    animateValue(popup_box_4_num1, 0, 96, 500);
+  });
 
-    poi_apollo.addEventListener("click", event => {
-      info_poi_apollo.style.display = "block";
-      info_poi_qianhai.style.display = "none";
-    });
-    
-  }
+    document.querySelector("#close-poi_1").addEventListener('click', function () {
+            info_poi_apollo.style.display = "none";
+  });
+    document.querySelector("#close-poi_2").addEventListener('click', function () {
+            info_poi_qianhai.style.display = "none";
+  });
+
+}
 
 );
-  
